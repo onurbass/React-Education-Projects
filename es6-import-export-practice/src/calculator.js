0c5099b05a9d0d42ee8b04b0d48e4f18ca247e9c
onurbass <onurbass.ob@gmail.com>
2023-09-19 02:41:30 +0300

    import export

---
+++ es6-import-export-practice/src/calculator.js
@@ -0,0 +1,18 @@
+var calculator;
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+export default calculator;
+export { add, multiply, subtract, divide };
